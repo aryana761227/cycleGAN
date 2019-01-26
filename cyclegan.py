c9453b988590643cdeb5181ca618d50265623fed
aryana <aryana761227@gmail.com>
2019-01-26 20:44:08 +0330

    test function is implemented

--- cyclegan.py
+++ cyclegan.py
@@ -1,12 +1,15 @@
+from __future__ import print_function, division
 from keras_contrib.layers.normalization import InstanceNormalization
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
+from keras.layers import Input, Dropout, Concatenate
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
+from keras.models import Model
 from keras.optimizers import Adam
 import datetime
+import matplotlib.pyplot as plt
 from data_loader import DataLoader
 import numpy as np
+import os
 
 
 class CycleGAN:
@@ -203,6 +206,41 @@ class CycleGAN:
                          np.mean(g_loss[3:5]),
                          np.mean(g_loss[5:6]),
                          elapsed_time))
+
+                # If at save interval => save generated image samples
+                if batch_i % sample_interval == 0:
+                    self.test(epoch, batch_i)
+
+    def test(self, epoch, batch_i):
+        os.makedirs('images/%s' % self.dataset_name, exist_ok=True)
+        r, c = 2, 3
+
+        imgs_A = self.data_loader.load_data(domain="A", batch_size=1, is_testing=True)
+        imgs_B = self.data_loader.load_data(domain="B", batch_size=1, is_testing=True)
+
+        # Translate images to the other domain
+        fake_B = self.g_AB.predict(imgs_A)
+        fake_A = self.g_BA.predict(imgs_B)
+        # Translate back to original domain
+        reconstr_A = self.g_BA.predict(fake_B)
+        reconstr_B = self.g_AB.predict(fake_A)
+
+        gen_imgs = np.concatenate([imgs_A, fake_B, reconstr_A, imgs_B, fake_A, reconstr_B])
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        titles = ['Original', 'Translated', 'Reconstructed']
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i, j].imshow(gen_imgs[cnt])
+                axs[i, j].set_title(titles[j])
+                axs[i, j].axis('off')
+                cnt += 1
+        fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
+        plt.close()
 
 
 if __name__ == '__main__':
